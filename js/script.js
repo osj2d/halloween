3995be8b060378536809354717f3e5d16dcbcf14
osj2d <juniorsavi97@gmail.com>
2023-10-14 14:52:36 -0300

    contador finalizado

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 import ScrollSuave from './scrollSuave.js'
+import Contador from './contador.js'
+
 const scrollSuave = new ScrollSuave('.links a[href^="#"], .linkBtn a[href^="#"]');
 scrollSuave.init();
+
+const contador = new Contador('.contador','2023-10-31T18:30:00');
+contador.init();
